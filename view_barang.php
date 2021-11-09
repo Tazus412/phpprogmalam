c939f8b6a1e68a3600117020c4a4724a56b73fca
Tazus412 <91581695+Tazus412@users.noreply.github.com>
2021-11-10 00:29:34 +0700

    Add files via upload

--- view_barang.php
+++ view_barang.php
@@ -1,14 +1,16 @@
-
 <?php 
 include("koneksi.php");
 $query_view=mysqli_query($koneksi,"select * from barang");
 
 ?>
+</br>
+<a href="input_anggota.php""btn btn-danger"> Tambah Anggota Lagi </a>
 <table class="table table-bordered" border="1">
 	<tr>
 		<td>id_barang</td>
 		<td>id_kategori</td>
 		<td>nama</td>
+		<td colspan="4"> Perubahan </td>
 	</tr>
 <?php 
 $no=1;
@@ -17,6 +19,8 @@ while($tampil=mysqli_fetch_array($query_view)){?>
 		<td><?php echo $tampil['id_barang'];?></td>
 		<td><?php echo $tampil['id_kategori'];?></td>
 		<td><?php echo $tampil['nama'];?></td>
+		<td><a href="edit_barang.php?id_barang=<?php echo $tampil['id_barang'];?>"> Edit </a></td>
+		<td><a href="hapus_barang.php?id_barang=<?php echo $tampil['id_barang'];?>"> Hapus </a></td>
 	</tr>
 <?php }?>
 </table>
